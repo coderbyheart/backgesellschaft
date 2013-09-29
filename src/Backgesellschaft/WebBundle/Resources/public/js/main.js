696cd206df52cba7b457df256b28f90c9f1209e7
Markus Tacker <m@coderbyheart.de>
2013-09-29 20:36:15 +0200

    Adds dummy product page.

--- src/Backgesellschaft/WebBundle/Resources/public/js/main.js
+++ src/Backgesellschaft/WebBundle/Resources/public/js/main.js
@@ -2,11 +2,49 @@ var IndexSlider = function () {
 
     $('.carousel').carousel();
 
-    console.log($('.carousel'));
-
     return {};
+};
+
+var Product = function (p) {
+
+    console.log(p);
+
+    var amount = p.find('input.amount:first')[0]
+
+    function getAmount() {
+        var a = parseInt(amount.value);
+        if (isNaN(a)) return 0;
+        return a;
+    }
+
+    function onAmountChange() {
+        if (getAmount() > 0) {
+            p.addClass("selected");
+        } else {
+            p.removeClass("selected");
+        }
+    }
+
+    $(amount).bind('change', onAmountChange);
+
+    p.click(function (ev) {
+        if (p.hasClass('selected')) return;
+        amount.value = getAmount() + 1;
+        onAmountChange();
+    });
+
+    return {}
+}
+
+var ProductSelector = function () {
+
+    $('.product').each(function (index, el) {
+        Product($(el));
+    });
+
 };
 
 $(document).ready(function () {
     IndexSlider();
+    ProductSelector();
 });
